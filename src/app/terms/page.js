11ec4890b99a9b8c9292bf63a51d9d41d5c60f6c
maanderz <mandyfung93@gmail.com>
2024-04-03 18:58:14 -0700

    added emphasis to terms

--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -1,14 +1,26 @@
+'use client'
+
 import styles from "../page.module.css";
 import localFont from 'next/font/local'
 import clsx from 'clsx';
+import { useState } from 'react';
 
 const myFont = localFont({ src: '../Instrument_Serif/InstrumentSerif-Regular.ttf' })
  
 export default function Terms() {
+  const [display, setDisplay] = useState(false);
+
   return (
     <main className={myFont.className}>
-      <main className={clsx(styles.main, styles.main_container)}>
-        Terms
+      <main className={clsx(styles.main, styles.main_container, styles.introduction)}>
+        <ol className={clsx(styles.introduction_1, styles.terms_container)}>
+            <li>
+                <span className={styles.terms} onClick={() => setDisplay((prevDisplay) => !prevDisplay)}>Computer literacy:</span>
+                <span className={display ? styles.terms_emphasized : '' }>Knowing basic computer fundamentals which can help purchasing, upgrading and using one.</span>  
+            </li>
+        
+        </ol>
+        <div className={styles.introduction_1}>Right</div>
       </main>
     </main>
   );
