40a5d9259f0d16eb0928507edad83a6fdda4f6f7
maanderz <mandyfung93@gmail.com>
2024-04-05 13:07:51 -0700

    added nav to trends

--- src/app/trends/page.js
+++ src/app/trends/page.js
@@ -1,15 +1,19 @@
 import styles from "../page.module.css";
 import localFont from 'next/font/local'
 import clsx from 'clsx';
+import Link from "next/link";
 
 const myFont = localFont({ src: '../Instrument_Serif/InstrumentSerif-Regular.ttf' })
  
 export default function Trends() {
   return (
-    <main className={myFont.className}>
-      <main className={clsx(styles.main, styles.main_container)}>
-        Trends
-      </main>
+    <main className={clsx(myFont.className, styles.introduction)}>
+      <section className={styles.title}>Trends</section>
+      <section className={styles.intro_links}>
+        <Link className={styles.intro_links_children} href="/">Home</Link>
+        <Link className={styles.intro_links_children} href="/terms">Terms</Link>
+        <Link className={styles.intro_links_children} href="/concepts">Concepts</Link>
+      </section>
     </main>
   );
 }
